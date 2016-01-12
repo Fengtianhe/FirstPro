f6ad28e50063170015cd6601c55f8f2fd2a6ade3
Fengtianhe <545704061@qq.com>
2016-01-12 21:15:13 +0800

    忘记密码找回

--- Application/Editor/Controller/AccountController.class.php
+++ Application/Editor/Controller/AccountController.class.php
@@ -2,9 +2,8 @@
 namespace Editor\Controller;
 use Think\Controller;
 class AccountController extends CommonController{
-
 	public function _initialize(){
-        parent::_initialize();
+		header("Content-type:text/html;charset=utf-8");
     }
     
 	public function index(){
@@ -38,6 +37,58 @@ class AccountController extends CommonController{
    			}
    		}
 
+	}
+	//忘记密码
+	public function forget(){
+		$this->assign('step','0');
+		$this->display();
+	}
+	public function step_1(){
+		$email=$_POST['email'];
+		$user=M('user');
+		$result=$user->where(array('eamil' => $email))->find();
+		if ($result) {
+			$uid = $result['id'];
+			$question_id=$result['aid'];
+			$question=M('answer')->where(array('aid' => $question_id))->find();
+			$problem=$question['atitle'];
+			$this->assign('uid',$uid);
+			$this->assign('problem',$problem);
+			$this->assign('step','1');
+			$this->display('forget');
+		}else{
+			$this->error('该用户不存在');
+		}
+		
+	}
+	public function step_2(){
+		$answer = md5($_POST['answer']);
+		$uid = $_POST['uid'];
+		$user=M('user');
+		$result=$user->where(array('id' => $uid))->find();
+		if ($answer==$result['answer']) {
+			$this->assign('uid',$uid);
+			$this->assign('step','2');
+			$this->display('forget');
+		}else{
+			$this->error('你输入的答案有误！请重新输入。');
+		}
+	}
+	public function step_3(){
+		if ($_POST['password']==$_POST['check_password']) {
+			$uid = $_POST['uid'];
+			$data['password']=md5($_POST['password']);
+			$user=M('user');
+			$result=$user->where(array('id' => $uid))->save($data);
+			if ($result) {
+				$this->success('修改成功，请登录！',U('editor/user/login'));
+			}
+			else{
+				$this->error('修改失败，或与原密码一样，请重试');
+			}
+		}else{
+			$this->error('两次输入的密码不一致！');
+		}
 	}
 
 
