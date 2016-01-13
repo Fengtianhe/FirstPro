cbc02270d79c0652aa908fb6c6a7df434aaa1157
Fengtianhe <545704061@qq.com>
2016-01-13 09:13:07 +0800

    城市定位  调试

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,8 +2,41 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+    public function getIPaddress(){
+        $IPaddress='';
+        if (isset($_SERVER)){
+            if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])){
+                $IPaddress = $_SERVER["HTTP_X_FORWARDED_FOR"];
+            } else if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+                $IPaddress = $_SERVER["HTTP_CLIENT_IP"];
+            } else {
+                $IPaddress = $_SERVER["REMOTE_ADDR"];
+            }
+        } else {
+            if (getenv("HTTP_X_FORWARDED_FOR")){
+                $IPaddress = getenv("HTTP_X_FORWARDED_FOR");
+            } else if (getenv("HTTP_CLIENT_IP")) {
+                $IPaddress = getenv("HTTP_CLIENT_IP");
+            } else {
+                $IPaddress = getenv("REMOTE_ADDR");
+            }
+        }
+        return $IPaddress;
+    }
+    public function taobaoIP(){
+        $clientIP = $this->getIPaddress();
+        $taobaoIP = 'http://ip.taobao.com/service/getIpInfo.php?ip='.$clientIP;
+        $IPinfo = json_decode(file_get_contents($taobaoIP));
+        $province = $IPinfo->data->region;
+        $city = $IPinfo->data->city;
+        //$data = $province.$city;
+        //return $data;
+        return $city;
+    }
     public function index()
     {  
+        $data = $this->taobaoIP();
+        $this->assign('city',$data);
         $offset = 0;
         $limit  = 4;
         $category = M('Category')->where(array('pid'=>'0'))->select();
