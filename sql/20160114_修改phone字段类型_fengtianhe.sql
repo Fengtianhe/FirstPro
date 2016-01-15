8b0a671b2b310c63955af34d66612782712614b5
my1977 <1977905246@qq.com>
2016-01-15 08:26:41 +0800

    首页精选推荐  图片路径

--- sql/20160114_修改phone字段类型_fengtianhe.sql
+++ sql/20160114_修改phone字段类型_fengtianhe.sql
@@ -1,1 +1,4 @@
-ALTER TABLE `zt_user` CHANGE `phone` `phone` VARCHAR( 11 ) NOT NULL DEFAULT '0' COMMENT '电话'
+ALTER TABLE `zt_user` CHANGE `phone` `phone` VARCHAR( 11 ) NOT NULL DEFAULT '0' COMMENT '电话';
+
+/*不重 zt_news 表 phone字段 也同时修改 */
+ALTER TABLE `zt_news` CHANGE `phone` `phone` VARCHAR(11) DEFAULT '' NOT NULL COMMENT '联系人电话'; 
