44b1b846b12e31922924a6309a68627ed3f63df8
Fengtianhe <545704061@qq.com>
2016-01-19 10:16:12 +0800

    我的收藏-显示

--- Application/Editor/Controller/AccountController.class.php
+++ Application/Editor/Controller/AccountController.class.php
@@ -93,8 +93,55 @@ class AccountController extends CommonController{
 	public function MyCollection(){
 		$uid = $_SESSION['me']['id'];
 		$Collection = M('Collect');
-		$datas = $Collection->where(array('user_id' => $uid))->select();
-		var_dump($datas);
+		$datas = $Collection->where(array('user_id' => $uid , 'status' => '0'))->select();
+		$count = count($datas);
+		for ($i=0; $i < $count; $i++) { 
+			$newsid = $datas[$i]['news_id'];
+			$newsinfo = M('news')->where(array('id' => $newsid))->find();
+			$datas[$i]['newstitle'] = $newsinfo['title'];
+			$datas[$i]['img'] = $newsinfo['img'];
+			$neworold = $newsinfo['neworold'];
+			switch ($neworold) {
+				case '9':
+					$datas[$i]['neworold'] = '九成新';
+					break;
+				case '8':
+					$datas[$i]['neworold'] = '八成新';
+					break;
+				case '7':
+					$datas[$i]['neworold'] = '七成新';
+					break;
+				case '6':
+					$datas[$i]['neworold'] = '六成新';
+					break;
+				case '5':
+					$datas[$i]['neworold'] = '五成新';
+					break;
+				default:
+					$datas[$i]['neworold'] = '其他';
+					break;
+			}
+			$is_del = $newsinfo['is_del'];
+			switch ($is_del) {
+				case '0':
+					$datas[$i]['is_del'] = '发布中';
+					break;
+				
+				default:
+					$datas[$i]['is_del'] = '已删除';
+					break;
+			}
+			$datas[$i]['price'] = $newsinfo['price'];
+			$userid = $datas[$i]['user_id'];
+			$userinfo = M('user')->where(array('id' => $userid))->find();
+			if ($userinfo['nickname']) {
+				$datas[$i]['username'] = $$userinfo['nickname'];
+			}else{
+				$datas[$i]['username'] = $$userinfo['email'];
+			}
+
+		}
+		$this->assign('datas',$datas);
 		$this->display();
 	}
 }
---
+++ Public/editor/js/collect_table.js
@@ -0,0 +1,10 @@
+    $(document).ready(function(){ //这个就是传说的ready    
+        $(".stripe tr").mouseover(function(){    
+           //如果鼠标移到class为stripe的表格的tr上时，执行函数    
+          $(this).addClass("over");}).mouseout(function(){    
+                //给这行添加class值为over，并且当鼠标一出该行时执行函数    
+                $(this).removeClass("over");}) //移除该行的class    
+      $(".stripe tr:even").addClass("alt");    
+        //给class为stripe的表格的偶数行添加class值为alt 
+        //www.divcss5.com 整理特效 
+      });   
