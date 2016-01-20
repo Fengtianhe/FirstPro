f1607617f87ccd589041c888226897f027ea1c7c
my1977 <1977905246@qq.com>
2016-01-21 02:02:40 +0800

    用户收藏信息 home

--- Application/Home/Controller/NewsController.class.php
+++ Application/Home/Controller/NewsController.class.php
@@ -12,8 +12,14 @@ class NewsController extends Controller {
     public function newsList(){
         $limit = 20;
         $category = M('Category')->where(array('pid'=>'0'))->select();
-        //用户收藏数据 应缓存  提高效率
-        //$collect  = M('Collect')->where(array('user_id'=>$_SESSION['me']['id']))->select();
+        //用户收藏数据 应缓存  提高效率 TODO 以后最好采用memcache做缓存
+        $collect_arr = array();
+        $collect  = M('Collect')->where(array('user_id'=>$_SESSION['me']['id'], 'status'=>0))->field('id, news_id')->select();
+        if (is_array($collect) && !empty($collect)) {
+            foreach ($collect as $key=>$value) {
+                $collect_arr[$value['news_id']] = 1;
+            }
+        }
         
         if($_REQUEST['category'])
         {
@@ -33,6 +39,7 @@ class NewsController extends Controller {
         $this->assign('list',$list);// 赋值数据集
         $this->assign('page',$show);// 赋值分页输出
         $this->assign('category',$category);
+        $this->assign('collect_arr',$collect_arr);// 收藏信息
     	$this->display();
     }
 
@@ -44,8 +51,15 @@ class NewsController extends Controller {
         $new_info['show_count'] = D('News')->showNum($id);
    		$new_content = M('Content')->where(array('news_id'=>$id))->find();
         //$new_info['images'] = json_decode($new_content['images'], true);
-   		$new_info['content'] = $new_content['content'];
+
+        $is_collect = 0;
+        $collect  = M('Collect')->where(array('user_id'=>$_SESSION['me']['id'], 'news_id'=>$id, 'status'=>0))->field('id, news_id')->find();
+   		if (is_array($collect) && !empty($collect)) {
+            $is_collect = 1;
+        } 
+        $new_info['content'] = $new_content['content'];
    		$this->assign('new_info',$new_info);
+        $this->assign('is_collect',$is_collect);
         $this->assign('new_images',json_decode($new_content['images'], true));
    		$this->display();
    	}
@@ -68,19 +82,31 @@ class NewsController extends Controller {
     {
         $data['news_id']     = I('id');
         $data['user_id']     = I('user_id',0);
+        $data['status']      = $collect_type = I('collect_type',0);
         $data['create_time'] = time();
 
         //避免重复收藏
-        if(!M('Collect')->where(array('news_id'=>$data['news_id'],'user_id'=>$data['user_id']))->find())
+        if($collect_info = M('Collect')->where(array('news_id'=>$data['news_id'],'user_id'=>$data['user_id']))->find())
         {
-            $result = M('Collect')->add($data);
-            $arr = array('status'=>'OK','info'=>'收藏成功','data'=>$result);
-            
+            if ($collect_type != $collect_info['status']) {
+                $result = M('Collect')->where(array('news_id'=>$data['news_id'],'user_id'=>$data['user_id']))->save(array('status'=>$collect_type));
+                if ($collect_type < 0 ) {
+                    $info = "收藏已取消"; 
+                } else {
+                    $info = "收藏成功"; 
+                }
+                
+            } else {
+                $info = "服务器出错啦"; 
+            }
         }
         else 
         {
-            $arr = array('status'=>'OK','info'=>'您已经收藏过本信息','data'=>0);
+            $result = M('Collect')->add($data);
+            $info = "收藏成功";        
         }
+        $arr = array('status'=>'OK','info'=>$info,'data'=>$result);
+        $arr['collect_type'] = $collect_type <0 ? 0 : -1;
         echo json_encode($arr);
     }
 }
