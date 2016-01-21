54487765e0ef63c842054bdc88039718c6bb4509
Fengtianhe <545704061@qq.com>
2016-01-21 11:27:05 +0800

    dwz-》后台框架

--- Application/Admin/Controller/CommonController.class.php
+++ Application/Admin/Controller/CommonController.class.php
@@ -2,9 +2,12 @@
 namespace Admin\Controller;
 use Think\Controller;
 class CommonController extends Controller {
-	public function __construct(){
-		if(!$_SESSION['me']['id']){
-			die('未登陆，或登陆失效');
-		}
-	}
+	// public function __construct(){
+	// 	if(!$_SESSION['me']['id']){
+	// 		die('未登陆，或登陆失效');
+	// 	}
+	// }
+	public function _initialize(){
+		header("Content-type:text/html;charset=utf-8");
+    }
 }
--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,13 +3,21 @@ namespace Admin\Controller;
 //use Think\Controller;
 use Admin\Controller;
 class IndexController extends CommonController {
-	public function __construct(){
-		parent::__construct();
-		echo "construct";
-	}
-    function test(){
-    	echo "123q";
-    	echo $_SESSION['me']['id'];
-    	$_SESSION['me']['id'] = '';
+	// public function __construct(){
+	// 	parent::__construct();
+	// 	echo "construct";
+	// }
+	public function _initialize(){
+		header("Content-type:text/html;charset=utf-8");
     }
+    public function index(){
+    	$url['xml_url'] = __ROOT__."/Public/admin/dwz.frag.xml";
+    	$url['login'] = U('Admin/User/login');
+    	$this->assign('url',$url);
+    	$this->display();
+    }
+    public function main(){
+    	$this->display();
+    }
+
 }
