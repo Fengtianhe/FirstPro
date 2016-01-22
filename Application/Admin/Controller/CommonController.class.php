90a93afc071891c517fd300c83bc7c6974e4e7dd
Fengtianhe <545704061@qq.com>
2016-01-22 10:59:43 +0800

    后台 - 用户管理

--- Application/Admin/Controller/CommonController.class.php
+++ Application/Admin/Controller/CommonController.class.php
@@ -10,4 +10,5 @@ class CommonController extends Controller {
 	public function _initialize(){
 		header("Content-type:text/html;charset=utf-8");
     }
+    
 }
---
+++ Application/Admin/Controller/UserController.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class UserController extends CommonController {
+	public function _initialize(){
+		header("Content-type:text/html;charset=utf-8");
+    }
+    public function main(){
+    	$date = date(m月d日,time());
+    	$weekarray=array("星期日","星期一","星期二","星期三","星期四","星期五","星期六");
+    	$week = $weekarray[date(w,time())];
+    	$dateweek = $date.'，'.$week ;
+    	$userlist = M('user')->select();
+    	$usercount = count($userlist);
+    	$this->assign('usercount',$usercount);
+    	$this->assign('userinfo',$userlist);
+    	$this->assign('date',$dateweek);
+    	$this->display();
+    }
+}
