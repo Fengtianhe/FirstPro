f3dafbc8b003597fd180e6e70d2e7a3b162f0dcb
my1977 <1977905246@qq.com>
2016-02-25 10:06:22 +0800

    新增查看用户协议

---
+++ Application/Home/Controller/ArticleController.class.php
@@ -0,0 +1,23 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ArticleController extends Controller {
+    public function index()
+    {
+        $id = I('id', 0);
+        $query = I('query', '');
+        if ($id || $query) {
+            if ($id) {
+                $where['id'] = $id;
+            }
+            if ($query) {
+                $where['query'] = $query;
+            }
+            $article = D('Article')->where($where)->find();
+            $this->assign('article', $article);
+            $this->display();
+        } else {
+            $this->error('内容不存在','/');
+        }
+    }
+}
