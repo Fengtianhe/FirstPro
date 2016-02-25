614efcfe4bc3428430315a64b9d3a64aecf94f18
my1977 <1977905246@qq.com>
2016-02-25 14:55:26 +0800

    修改留言表注释

--- sql/20160222_留言表主键自增_maying.sql
+++ sql/20160222_留言表主键自增_maying.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `zt_contact` CHANGE `id` `id` INT(11) NOT NULL AUTO_INCREMENT; 
+ALTER TABLE `zt_contact` CHANGE `status` `status` TINYINT(1) DEFAULT 0 NOT NULL COMMENT '0:正常 1：已回复 2：标记异常'; 
